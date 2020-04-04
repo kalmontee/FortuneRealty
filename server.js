6750fa1dee7f1fb437223f07321c1fb484473f7c
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-04-04 17:37:07 -0400

    Setup files for backend development.

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 // const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -7,12 +6,14 @@ const PORT = process.env.PORT || 8080;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+let routes = require("./controllers/appController"); 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-// app.use(routes);
+app.use(routes);
 
 // Start the API server
 app.listen(PORT, () => console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`));
