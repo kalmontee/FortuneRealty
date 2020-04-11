2745dc742f1b546ee247cabd15e2c29268f1d34e
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-04-11 12:16:16 -0400

    Changed the NavBar design. Set the NavBar to a display flex, and text align center.

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -5,9 +5,6 @@ import './navbar.css'
 const NavBar = () => {
   return (
     <header className={window.location.pathname === "/" ? "home-navbar" : "navbar-pages"}>
-      <Link to="/" className={window.location.pathname === "/" ? "active navbar-title" : "navbarLink navbar-title"}>
-        Fortune Realty
-      </Link>
 
       <Link to="/about-us" className={window.location.pathname === "/about-us" ? "active" : "navbarLink"}>
         About Us
@@ -15,6 +12,10 @@ const NavBar = () => {
 
       <Link to="/buy" className={window.location.pathname === "/buy" ? "active" : "navbarLink"}>
         Buy
+      </Link>
+
+      <Link to="/" className={window.location.pathname === "/" ? "active navbar-title" : "navbarLink navbar-title"}>
+        Fortune Realty
       </Link>
 
       <Link to="/sell" className={window.location.pathname === "/sell" ? "active" : "navbarLink"}>
