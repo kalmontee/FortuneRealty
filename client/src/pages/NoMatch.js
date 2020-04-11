0d27bb146ac4b651ede27f7b7262d161c04b6407
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-04-11 12:23:42 -0400

    Few minor styling for the 404 page.

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import { Container } from '../components/Grid/Grid';
-import NavBar from '../components/NavBar/NavBar';
+import { Row } from '../components/Grid/Grid';
 
 function NoMatch() {
   const divStyle = {
-    textAlign: 'center'
+    // backgroundDiv: {
+    //   background: "url()",
+    //   backgroundSize: 'cover',
+    //   width: '100%',
+    //   height: '750px',
+    // },
+
+    emojiStyle: {
+      textAlign: 'center',
+      marginTop: '90px',
+      marginBottom: '20px',
+      fontSize: '10rem',
+    },
+
+    notFound: {
+      textAlign: 'center',
+      fontWeight: '400',
+    }
+
   }
   return (
-    <Container>
-      <NavBar />
-      <h1 style={divStyle}>404 Page Not Found</h1>
-      <h1 style={divStyle}>
-        <span role="img" aria-label="Face With Rolling Eyes Emoji">
-          🙄
-        </span>
+    <div style={divStyle.backgroundDiv}>
+      <h1 style={divStyle.emojiStyle}>4<span role="img" aria-label="Face With Rolling Eyes Emoji">😔</span>4</h1>
+      
+      <h1 style={divStyle.notFound}>
+      OOPS! PAGE NOT FOUND
       </h1>
-    </Container>
+    </div>
   );
 }
+// 🙄
 
 export default NoMatch;
