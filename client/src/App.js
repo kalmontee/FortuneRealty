eb0f2e4976d2d35d6b891198e3057329a36a7f08
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-04-11 12:13:15 -0400

    Included two new components pages.

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,28 @@ import Sell from './pages/Sell';
 import Testimonials from './pages/Testimonials';
 import Contact from './pages/Contact';
 import NoMatch from './pages/NoMatch';
-import logo from './logo.svg';
+import { RubiJimenez, CarlosMejia } from './pages/Agents';
+import NavBar from './components/NavBar/NavBar';
 import './App.css';
+// import logo from './logo.svg';
 
 function App() {
   return (
     <Router>
+      <>
+        <NavBar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about-us" component={About} />
+          <Route exact path="/about-us/Rubi+Jimenez" component={RubiJimenez} />
+          <Route exact path="/about-us/Carlos+Mejia" component={CarlosMejia} />
           <Route exact path="/buy" component={Buy} />
           <Route exact path="/sell" component={Sell} />
           <Route exact path="/testimonials" component={Testimonials} />
           <Route exact path="/contact" component={Contact} />
           <Route component={NoMatch} />
         </Switch>
+      </>
     </Router>
   );
 }
