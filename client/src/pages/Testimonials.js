225a1687b6ef2c7a01641a4738d53af02889e4dd
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-04-11 12:25:03 -0400

    Removed NavBar Component.

--- client/src/pages/Testimonials.js
+++ client/src/pages/Testimonials.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import NavBar from '../components/NavBar/NavBar';
 
 class Testimonials extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
+      <>
         <h1>Testimonials Page</h1>
-      </div>
+      </>
     )
   }
 }
