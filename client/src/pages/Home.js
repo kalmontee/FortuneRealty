d3a61971c41df58a1cb0ac64a0b3eed66d38a4a1
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-04-11 12:21:54 -0400

    Removed the NavBar Component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import NavBar from '../components/NavBar/NavBar';
 import { HomeContainer } from '../components/Grid/Grid';
 
 class Home extends Component {
   render() {
     return (
       <HomeContainer>
-        <NavBar />
         <h1>Hello World</h1>
       </HomeContainer>
     )
