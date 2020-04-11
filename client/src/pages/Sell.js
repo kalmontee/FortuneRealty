5c2fb885f9651d86cc1a3d3a38e87009f2e3622a
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-04-11 12:24:34 -0400

    Removed NavBar Component.

--- client/src/pages/Sell.js
+++ client/src/pages/Sell.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import NavBar from '../components/NavBar/NavBar'
 
 class Sell extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
+      <>
         <h1>Sell Page</h1>
-      </div>
+      </>
     )
   }
 }
